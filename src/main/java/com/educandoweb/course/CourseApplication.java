e16c28b486aadbb30be5fce1ecf4be67d32ec4d9
Paulo Souza <paulohrs30@gmail.com>
2021-06-11 15:22:52 -0300

    User entity and resource

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.example.algamoney.api;
+package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
