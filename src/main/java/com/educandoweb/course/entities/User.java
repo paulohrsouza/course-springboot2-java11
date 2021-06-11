3dca99f55f5185d1ed9d611e686eccaedfdd632c
Paulo Souza <paulohrs30@gmail.com>
2021-06-11 17:41:50 -0300

    order pt1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -19,6 +24,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String passowrd;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	public User() {
 		
@@ -60,6 +69,10 @@ public class User implements Serializable {
 	public String getPhone() {
 		return phone;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	public void setPhone(String phone) {
 		this.phone = phone;
@@ -97,5 +110,7 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 }
